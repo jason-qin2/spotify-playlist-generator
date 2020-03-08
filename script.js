762a42046bfec92ee6dc2b942a5fdc68cce8d49e
jasonator1228 <qinzechen@hotmail.com>
2020-03-08 17:05:28 -0400

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+const app = {};
+
+app.events = function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        let artists = $('input[type=search]').val();
+        console.log(artists);
+    }
+};
+
+app.init = function() {
+    app.events();
+};
+
+$(app.init)
