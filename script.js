832140d5e9580cbc0c5b9572f8db84aad8473805
jasonator1228 <qinzechen@hotmail.com>
2020-03-08 19:58:00 -0400

    updates

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const app = {};
+app.apiUrl = 'https://api.spotify.com/v1/';
 
 app.events = function() {
     $('form').on('submit', function(e) {
         e.preventDefault();
         let artists = $('input[type=search]').val();
-        console.log(artists);
-    }
+        artists = artists.split(',');
+        let search = artists.map(artistName => app.searchArtist(artistName));
+        console.log(search);
+    });
 };
+
+app.searchArtist = (artistName) => $.ajax({
+    url: '${app.apiUrl}search',
+    method:'GET',
+    dataType: 'json',
+    data: {
+        q: artistName,
+        type: 'artist'
+    }
+});
 
 app.init = function() {
     app.events();
